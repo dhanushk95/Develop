8c0845f02679141fd3dc4ace0f79d42907863562
dhanushk95 <45707146+dhanushk95@users.noreply.github.com>
2020-09-02 21:31:33 +0530

    Update SumArray.java

--- HackerRank/SumArray.java
+++ HackerRank/SumArray.java
@@ -1,1 +1,24 @@
+package Hackerrank;
+
+public class SumArray 
+	{
+	  
+	 static int Sum(int ar[]) {
+
+	    int sum =0;
+	    for(int i=0; i<ar.length; i++)
+      {
+		      sum += ar[i];
+	    }
+       return sum;
+   }
+	public static void main(String args[])
+	 {
+          int ar[] = {1,9,7,3,5};
+          Sum(ar);  
+            
+          System.out.println(Sum(ar));
+	 }
+
+}
 
