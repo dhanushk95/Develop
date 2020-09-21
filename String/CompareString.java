5f6beaa5fb00c4ff1e92079ddeea6f7101b81c7c
Dhanush Kumar <45707146+dhanushk95@users.noreply.github.com>
2020-09-21 20:55:55 +0530

    Update CompareString

--- String/CompareString.java
+++ String/CompareString.java
@@ -2,23 +2,25 @@ package String;
 
 public class CompareString {
 	
-	static boolean ignoreCase(String str1, String str2)
+	static boolean isEqual(String str1, String str2)
 	{
-		str1 = str1.toUpperCase(); 						//Ignore this, to compare for case sensitive
-		str2 = str2.toUpperCase();
-		
-		int x = str1.compareTo(str2);
+		int x = str1.compareToIgnoreCase(str2); 		//Method 1
 		
 		if(x != 0)
 			return false;
-		
+									//Remove IgnoreCase for case sensitive
 		else return true;
+		
+		/*if(str1.equalsIgnoreCase(str2))             		//Method 2
+			return true;
+		
+		else return false;*/
 	}
 	public static void main(String args[])
 	{
 		String a = "kilo", b= "gram";
 		
-		if(ignoreCase(a, b))
+		if(isEqual(a, b))
 			System.out.println("Strings are same");
 		
 		else System.out.println("Different");
