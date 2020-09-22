aaa9aaf3168f72ae03d8acaec136b00af9acc428
Dhanush Kumar <45707146+dhanushk95@users.noreply.github.com>
2020-09-22 22:00:24 +0530

    Remove the Duplicate characters

---
+++ String/RemoveDuplicates.java
@@ -0,0 +1,28 @@
+package String;
+
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicates {
+	
+	static void removeDuplicates(String str)
+	{
+		LinkedHashSet<Character> set = new LinkedHashSet<>();
+		
+		for(int i=0; i<str.length(); i++)
+		{
+			set.add(str.charAt(i));
+		}
+		
+		for(Character ch : set)
+			System.out.print(ch);		
+	}
+	
+	public static void main(String args[])
+	{
+		String str = "Never give up";
+		
+		removeDuplicates(str);
+		
+	}
+
+}
