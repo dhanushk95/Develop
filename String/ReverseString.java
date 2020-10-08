40d3979cb15b5e3db9b8e1b510ea7e5ac71b0e30
Dhanush Kumar <45707146+dhanushk95@users.noreply.github.com>
2020-10-08 20:53:54 +0530

    Update Reverse string

--- String/ReverseString.java
+++ String/ReverseString.java
@@ -3,7 +3,7 @@ package String;
 public class ReverseString {
 	
 	static void reverse(String str)
-	/*{												                                    //Method 1 - Convert String to char array
+	/*{									  //Method 1 - Convert String to char array
 		char[] ch = str.toCharArray();
 	
 		for(int i =ch.length -1; i>=0; i--)
@@ -14,7 +14,7 @@ public class ReverseString {
 	{
 		StringBuffer sb = new StringBuffer(str); 	                  //Method 2 - Use StringBuffer
 		
-		sb.reverse(); 								                              //Built-in function reverse
+		sb.reverse(); 							  //Built-in function reverse
 		System.out.println(sb);
 	}
 	public static void main(String args[])
