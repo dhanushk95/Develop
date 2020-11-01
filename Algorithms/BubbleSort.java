18cc1621bfee94089ea13690f2427908aa371168
Dhanush Kumar <45707146+dhanushk95@users.noreply.github.com>
2020-11-01 22:40:47 +0530

    Update BubbleSort.java

--- Algorithms/BubbleSort.java
+++ Algorithms/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
 	
 	static void sort(int arr[])
 	{
-		for(int i=0; i<arr.length; i++)
+		for(int i=0; i<arr.length-1; i++)
 		{
 			for(int j=0; j<arr.length-1; j++)
 			{
